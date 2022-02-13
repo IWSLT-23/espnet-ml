e3c6f621c663f7889806a03ce1eb0a5b6679843d
brianyan918 <brianyan918@gmail.com>
2022-02-13 16:34:28 -0500

    hier scripts

--- egs/aesrc2020/asr1/local/make_hier_json.py
+++ egs/aesrc2020/asr1/local/make_hier_json.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # add token gt using dict
     parser.add_argument("--input", required=True)
     parser.add_argument("--inter1", required=True)
-    parser.add_argument("--inter2", required=True)
+    parser.add_argument("--inter2", required=False, default="nothing")
     parser.add_argument("--tag", required=True)
     args = parser.parse_args()
 
@@ -19,17 +19,25 @@ if __name__ == "__main__":
     with open(args.inter1, "r", encoding="utf-8") as f:
         inter1 = json.load(f)
 
-    with open(args.inter2, "r", encoding="utf-8") as f:
-        inter2 = json.load(f)
+    if args.inter2 == "nothing":
+        inter2 = None
+    else:
+        with open(args.inter2, "r", encoding="utf-8") as f:
+            inter2 = json.load(f)
 
     for u in data["utts"]:
+        # if u not in inter1["utts"].keys():
+        #     # data["utts"].pop(u)
+        #     print(u)
+        #     continue
         temp_dict = copy.copy(inter1["utts"][u]["output"][0])
         temp_dict["name"] = "target2"
         data["utts"][u]["output"].append(temp_dict)
 
-        temp_dict = copy.copy(inter2["utts"][u]["output"][0])
-        temp_dict["name"] = "target3"
-        data["utts"][u]["output"].append(temp_dict)
+        if inter2 != None:
+            temp_dict = copy.copy(inter2["utts"][u]["output"][0])
+            temp_dict["name"] = "target3"
+            data["utts"][u]["output"].append(temp_dict)
 
     with open(args.input+".hier"+args.tag, "wb") as f:
         f.write(json.dumps(data, indent=4, ensure_ascii=False, sort_keys=True).encode("utf-8"))
--- egs/aesrc2020/asr1/local/prepare_acc_hier.sh
+++ egs/aesrc2020/asr1/local/prepare_acc_hier.sh
@@ -9,27 +9,39 @@ inter1=$3
 inter2=$4
 
 # separate accents in inter layers 
-python local/separate_accents.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json
-python local/separate_accents.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json
-python local/separate_accents.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter2}.json
-python local/separate_accents.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter2}.json
+#python local/separate_accents.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json
+#python local/separate_accents.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json
+#python local/separate_accents.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter2}.json
+#python local/separate_accents.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter2}.json
 
 # add aid tags
-python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json.acc
-python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json.acc
-python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter2}.json.acc
-python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter2}.json.acc
-python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json
-python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json
+#python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json.acc
+#python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json.acc
+#python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter2}.json.acc
+#python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter2}.json.acc
+#python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json
+#python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json
 
 # # combine into one
 tag=${inter1}-${inter2}
-python local/make_hier_json.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json.aid \
---inter1 ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json.acc.aid \
---inter2 ${dumpdir}/train_sp/deltafalse/data_unigram${inter2}.json.acc.aid --tag ${tag}
-python local/make_hier_json.py --input ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json.aid \
---inter1 ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json.acc.aid \
---inter2 ${dumpdir}/dev/deltafalse/data_unigram${inter2}.json.acc.aid --tag ${tag}
+#python local/make_hier_json.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json.aid \
+#--inter1 ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json.acc.aid \
+#--inter2 ${dumpdir}/train_sp/deltafalse/data_unigram${inter2}.json.acc.aid --tag ${tag}
+#mv ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json.aid.hier${inter1}-${inter2} \
+#    ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json.aid.acc.hier${inter1}-${inter2}
+#python local/make_hier_json.py --input ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json.aid \
+#--inter1 ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json.acc.aid \
+#--inter2 ${dumpdir}/dev/deltafalse/data_unigram${inter2}.json.acc.aid --tag ${tag}
+#mv ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json.aid.hier${inter1}-${inter2} \
+#    ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json.aid.acc.hier${inter1}-${inter2}
 
-echo ".aid.hier${tag}"
+python local/add_aid_tag.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json
+python local/add_aid_tag.py --input ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json
+python local/make_hier_json.py --input ${dumpdir}/train_sp/deltafalse/data_unigram${nbpe}.json.aid \
+--inter1 ${dumpdir}/train_sp/deltafalse/data_unigram${inter1}.json.aid \
+--tag ${tag}
+python local/make_hier_json.py --input ${dumpdir}/dev/deltafalse/data_unigram${nbpe}.json.aid \
+--inter1 ${dumpdir}/dev/deltafalse/data_unigram${inter1}.json.aid \
+--tag ${tag}
+echo ".aid.acc.hier${tag}"
 exit 0;
