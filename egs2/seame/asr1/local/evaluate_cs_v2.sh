caae4522a7a28bacda0025c6982e8e26128aaded
brianyan918 <byan@test1.cm.gemini>
2022-07-06 14:21:07 -0400

    scoring scripts

--- egs2/seame/asr1/local/evaluate_cs_v2.sh
+++ egs2/seame/asr1/local/evaluate_cs_v2.sh
@@ -23,28 +23,28 @@ python local/create_trn_format.py --hyp $hyp --ref $ref
 # sclite
 echo "all (no lid)"
 sclite \
-    -r "${hyp}.all" trn \
-    -h "${ref}.all" trn \
+    -r "${ref}.all" trn \
+    -h "${hyp}.all" trn \
     -i rm -o dtl stdout > "${hyp}.all.result"
 grep "Percent Total Error" "${hyp}.all.result"
 
 echo "en only"
 sclite \
-    -r "${hyp}.en" trn \
-    -h "${ref}.en" trn \
+    -r "${ref}.en" trn \
+    -h "${hyp}.en" trn \
     -i rm -o dtl stdout > "${hyp}.en.result"
 grep "Percent Total Error" "${hyp}.en.result"
 
 echo "zh only"
 sclite \
-    -r "${hyp}.zh" trn \
-    -h "${ref}.zh" trn \
+    -r "${ref}.zh" trn \
+    -h "${hyp}.zh" trn \
     -i rm -o dtl stdout > "${hyp}.zh.result"
 grep "Percent Total Error" "${hyp}.zh.result"
 
 echo "lid only"
 sclite \
-    -r "${hyp}.lid" trn \
-    -h "${ref}.lid" trn \
+    -r "${ref}.lid" trn \
+    -h "${hyp}.lid" trn \
     -i rm -o dtl stdout > "${hyp}.lid.result"
 grep "Percent Total Error" "${hyp}.lid.result"
